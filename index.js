bc71f92857024a837e6014b6a430d272c3f793cd
amJayem <csejayem19@gmail.com>
2022-12-25 23:36:53 +0600

    fetching sectors data from mongodb

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async() =>{
+    
+    try{
+        const sectorCollection = client.db('sector-selector').collection('sectors');
+        app.get('/sectors', async(req,res)=>{
+            const sectors = await sectorCollection.find({}).toArray();
+            // console.log(sectors);
+            res.send(sectors);
+        })
+
+    }
+    finally{}
+}
+
+run().catch((e) => {
+    console.error("run error => ", e.message);
+  });
+  
+  app.get("/", (req, res) => {
+    res.send("sector-selector server is running...");
+  });
+  
+  app.listen(port, () => {
+    console.log("server running on: ", port);
+  });
+  
